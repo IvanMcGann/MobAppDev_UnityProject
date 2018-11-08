5c47c811d3afa471beecafed045a6c2cf0b3f1db
Ivan McGann <g00340138@gmit.ie>
2018-11-08 15:04:09 +0000

    Added Pooling
    Lost a lot of updates as the github desktiop app wasn't connecting to Github as I had Unity open

--- Veganator_Game/Assets/Scripts/PlatformDestroyer.cs
+++ Veganator_Game/Assets/Scripts/PlatformDestroyer.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformDestroy : MonoBehaviour {
+public class PlatformDestroyer : MonoBehaviour {
 
     public GameObject platformDesPoint;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    private void Start () {
         platformDesPoint = GameObject.Find("PlatformDesPoint");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (transform.position.x > platformDesPoint.transform.position.x)
+        if (transform.position.x < platformDesPoint.transform.position.x)
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
+
         }
 	}
 }
--- Veganator_Game/Assets/Scripts/PlatformGenerator.cs
+++ Veganator_Game/Assets/Scripts/PlatformGenerator.cs
@@ -10,18 +10,78 @@ public class PlatformGenerator : MonoBehaviour {
 
     public float distanceBetween;
 
+
+    public float distanceBetweenMin;
+    public float distanceBetweenMax;
+
+
+    //public GameObject[] thePlatforms;
+    private int platformSelector;
+    private float[] platformWidths;
+
+    public ObjectPooler[] theObjectPools;
+
+    // for jumping
+
+    private float minHeight;
+    private float maxHeight;
+    public float maxHeightChange;
+    public float heightChange;
+    public Transform maxHeightPoint;
+
+
+
+
 	// Use this for initialization
 	void Start () {
-        platformWidth = examplePlatform.GetComponent<BoxCollider2D>().size.x;
+        // platformWidth = examplePlatform.GetComponent<BoxCollider2D>().size.x;
+        platformWidths = new float[theObjectPools.Length];
+
+        for(int i = 0; i< theObjectPools.Length; i++)
+        {
+            platformWidths[i] = theObjectPools[i].pooledObject.GetComponent<BoxCollider2D>().size.x;
+        }
+
+        minHeight = transform.position.y;
+        maxHeight = maxHeightPoint.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
         if (transform.position.x < generationPoint.position.x)
         {
-            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
-            //copy
-            Instantiate(examplePlatform, transform.position, transform.rotation);
+            distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+            platformSelector = Random.Range(0, theObjectPools.Length);
+
+
+            heightChange = transform.position.y + Random.Range(maxHeightChange, -maxHeightChange);
+
+            if(heightChange > maxHeight)
+            {
+                heightChange = maxHeight;
+            }
+
+            else if(heightChange < minHeight)
+            {
+                heightChange = minHeight;
+            }
+
+            //moves object
+            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] /2)+ distanceBetween, heightChange, transform.position.z);
+
+
+            //copy, remove for pools
+            //Instantiate(thePlatforms[platformSelector], transform.position, transform.rotation);
+            
+            GameObject newPlatform = theObjectPools[platformSelector].GetPooledObject();
+
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive(true);
+
+            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2), transform.position.y, transform.position.z);
+
         }
-	}
+    }
 }
